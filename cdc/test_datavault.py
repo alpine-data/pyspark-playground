9b73cb6d5ac4d7b315a254ecc7bd3989138c80b5
Michael Wellner <michaelwellner@kpmg.com>
2022-02-07 17:51:58 +0100

    sc

--- cdc/test_datavault.py
+++ cdc/test_datavault.py
@@ -18,6 +18,7 @@ class Columns:
     OPERATION = "operation"
 
 class Operation:
+    SNAPSHOT = 0
     DELETE = 1
     CREATE = 2
     BEFORE_UPDATE = 3
@@ -25,12 +26,19 @@ class Operation:
 
 def create_movies(spark: SparkSession) -> DataFrame:
     t1 = datetime.now()
+    t0 = t1 - datetime.now(days=1)
     t2 = t1 + timedelta(hours=3, minutes=17)
     t3 = t2 + timedelta(minutes=42) 
-    t4 = t3 + timedelta(days=1)
+    t4 = t3 + timedelta(hours=6)
+    t5 = t1 + timedelta(days=1, minutes=42)
 
 
     data = [
+        [
+            (Operation.SNAPSHOT, t0, 1, "The Shawshank Redemption", 1994, "Frank Darabont", 9.3, 64),
+            (Operation.SNAPSHOT, t0, 2, "The Godfather", 1972, "Francis Ford Coppola", 9.2, 94),
+        ],
+    [
         # $operation, $load_date, id, name, year, director, rating, rank
         (Operation.CREATE, t1, 1, "The Shawshank Redemption", 1994, "Frank Darabont", 9.3, 64),
         (Operation.CREATE, t1, 2, "The Godfather", 1972, "Francis Ford Coppola", 9.2, 94),
@@ -43,7 +51,11 @@ def create_movies(spark: SparkSession) -> DataFrame:
 
         (Operation.DELETE, t3, 1, "The Shawshank Redemption", 1994, "Frank Darabont", 9.3, 64),
         (Operation.UPDATE, t4, 3, "The Dark Knight", 2008, "Christopher Nolan", 9.0, 101)
-    ]
+    ],
+    [
+        (Operation.UPDATE, t5, 3, "The Dark Knight", 2008, "Christopher Nolan", 9.0, 101),
+        (Operation.DELETE, t3, 1, "The Shawshank Redemption", 1994, "Frank Darabont", 9.3, 64)
+    ]]
 
     return spark \
         .createDataFrame(data, [Columns.OPERATION, Columns.LOAD_DATE, "id", "name", "year", "director", "rating", "rank"]) \
@@ -52,16 +64,39 @@ def create_movies(spark: SparkSession) -> DataFrame:
 
 def prepare_hub(staging: DataFrame, business_key_columns: List[str]) -> DataFrame:
     # Löschen getrennt behandeln 
-    
+
     return staging \
-        .withColumn(Columns.LAST_SEEN_DATE, staging[Columns.LOAD_DATE]) \
         .alias("l") \
-        .join(staging.alias("r"), Columns.HKEY) \
-        .where("l.load_date <= r.load_date AND l.operation != 1") \
-        .withColumn("diff", F.col("r.load_date").cast("long") - F.col("l.load_date").cast("long")) \
-        .withColumn("test", F.abs(F.hash("l.hkey"))) \
-        .groupBy("l.hkey", "test", "l.name", "l.year") \
-        .agg(F.min(F.col("l.load_date")).alias("load_date"), F.max(F.col("r.load_date")).alias("last_seen_date")) \
+        .groupBy("l.hkey", "l.name", "l.year") \
+        .agg(F.min(F.col("l.load_date")).alias("load_date"), F.max(F.col("l.load_date")).alias("last_seen_date")) \
+
+def test_datavault_transformatios(spark: SparkSession):
+    # Tabellen: Movies, Actors, Casts
+    df: List[Tuple[DataFrame, DataFrame, DataFrame]] = create_movies(spark)
+
+    # Day 1
+    day01 = df[0]
+    df_hub_movies = load_hub("HUB__movies", day01[0], business_key_columns=["name", "year"])
+    df_hub_actors = load_hub("HUB__actor", day01[0], business_key_columns=["firstname", "lastname", "dateOfBirth"])
+
+    df_link = load_link("LINK_casts", link_table = day01[2], linked_entities = [day01[0], day01[1]])
+
+    df_sat_movies = load_sat("HUB__movies", day01[0], business_key_columns=["name", "year"])
+    df_sat_actors = load_sat("HUB__movies", day01[0], business_key_columns=["name", "year"])
+
+    # Assert content of df_hub... and df_link and df_sat...
+
+    # Day 2
+    day02 = df[1]
+    df_hub_movies = load_hub("HUB__movies", day01[0], business_key_columns=["name", "year"], current = df_hub_movies)
+    df_hub_actors = load_hub("HUB__actor", day01[0], business_key_columns=["firstname", "lastname", "dateOfBirth"], current = df_hub_actors)
+
+    df_link = load_link("LINK_casts", link_table = day01[2], linked_entities = [day01[0], day01[1]], current = df_link)
+
+    df_sat_movies = load_sat("HUB__movies", day01[0], business_key_columns=["name", "year"], current = df_sat_movies)
+    df_sat_actors = load_sat("HUB__movies", day01[0], business_key_columns=["name", "year"], current = df_sat_actors)
+
+    # Assert updated content ...
 
 def test_transform(spark: SparkSession):
     df = create_movies(spark)
