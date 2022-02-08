ffc0a80d0438108d47006abaa0ebd1a903ec4eb4
janwithb@gmail.com <janwithb@gmail.com>
2022-02-08 09:22:26 +0100

    extended cdc test data model

--- cdc/test_datavault.py
+++ cdc/test_datavault.py
@@ -1,8 +1,8 @@
 from datetime import datetime, timedelta
 from select import select
 from time import time
-from chispa import *
 
+from chispa import *
 from delta.tables import *
 
 import pyspark.sql.functions as F
@@ -17,6 +17,7 @@ class Columns:
     LOAD_DATE = "load_date"
     OPERATION = "operation"
 
+
 class Operation:
     SNAPSHOT = 0
     DELETE = 1
@@ -24,42 +25,133 @@ class Operation:
     BEFORE_UPDATE = 3
     UPDATE = 4
 
+
 def create_movies(spark: SparkSession) -> DataFrame:
     t1 = datetime.now()
-    t0 = t1 - datetime.now(days=1)
+    t0 = t1 - timedelta(days=1)
     t2 = t1 + timedelta(hours=3, minutes=17)
     t3 = t2 + timedelta(minutes=42) 
     t4 = t3 + timedelta(hours=6)
     t5 = t1 + timedelta(days=1, minutes=42)
 
+    data = [
+        [
+            # $operation, $load_date, id, name, year, director, rating, rank
+            (Operation.SNAPSHOT, t0, 1, "The Shawshank Redemption", 1994, "Frank Darabont", 9.3, 64),
+            (Operation.SNAPSHOT, t0, 2, "The Godfather", 1972, "Francis Ford Coppola", 9.2, 94),
+            (Operation.SNAPSHOT, t0, 3, "The Dark Knight", 2008, "Christopher Nolan", 9.0, 104),
+            (Operation.SNAPSHOT, t0, 4, "Star Wars: Episode V", 1980, "Irvin Kershner", 8.7, 485)
+        ],
+        [
+            (Operation.CREATE, t2, 5, "Pulp Fiction", 1994, "Quentin Terintino", 8.9, 138),
+            (Operation.CREATE, t2, 6, "Schindler's List", 1993, "Steven Spielberg", 8.6, 145),
+            (Operation.CREATE, t2, 7, "Inception", 2010, "Christopher Nolan", 8.3, 210),
+            (Operation.UPDATE, t2, 2, "The Dark Knight", 2008, "Christopher Nolan", 9.1, 97),
+            (Operation.UPDATE, t3, 3, "Star Wars: Episode V", 1980, "Irvin Kershner", 8.4, 500)
+            (Operation.UPDATE, t3, 5, "The Shawshank Redemption", 1994, "Frank Darabont", 9.2, 67),
+            (Operation.UPDATE, t4, 3, "The Godfather", 1972, "Francis Ford Coppola", 9.1, 96),
+            (Operation.UPDATE, t4, 5, "Schindler's List", 1993, "Steven Spielberg", 8.8, 125),
+            (Operation.DELETE, t4, 1, "Star Wars: Episode V", 1980, "Irvin Kershner", 8.4, 500)
+            (Operation.DELETE, t4, 1, "The Dark Knight", 2008, "Christopher Nolan", 9.1, 97)
+        ],
+        [
+            (Operation.UPDATE, t5, 5, "The Shawshank Redemption", 1994, "Frank Darabont", 9.1, 69),
+            (Operation.UPDATE, t5, 3, "The Godfather", 1972, "Francis Ford Coppola", 8.9, 103),
+            (Operation.UPDATE, t5, 5, "Schindler's List", 1993, "Steven Spielberg", 8.3, 210),
+            (Operation.CREATE, t5, 1, "Star Wars: Episode V", 1980, "Irvin Kershner", 8.4, 500)
+        ]
+    ]
+
+    return [
+        spark.createDataFrame(b, [Columns.OPERATION, Columns.LOAD_DATE, "id", "name", "year", "director", "rating", "rank"]) \
+        .withColumn(Columns.HKEY, DataVaultFunctions.hash(["name", "year"])) for b in data
+    ]
+
+
+def create_actors(spark: SparkSession) -> DataFrame:
+    t1 = datetime.now()
+    t0 = t1 - timedelta(days=1)
+    t2 = t1 + timedelta(hours=3, minutes=17)
+    t3 = t2 + timedelta(minutes=42) 
+    t4 = t3 + timedelta(hours=6)
+    t5 = t1 + timedelta(days=1, minutes=42)
 
     data = [
         [
-            (Operation.SNAPSHOT, t0, 1, "The Shawshank Redemption", 1994, "Frank Darabont", 9.3, 64),
-            (Operation.SNAPSHOT, t0, 2, "The Godfather", 1972, "Francis Ford Coppola", 9.2, 94),
+            # $operation, $load_date, id, name
+            (Operation.SNAPSHOT, t0, 1, "Tim Robbins"),
+            (Operation.SNAPSHOT, t0, 2, "Morgan Freeman"),
+            (Operation.SNAPSHOT, t0, 3, "Bob Gunton"),
+            (Operation.SNAPSHOT, t0, 4, "William Sadler"),
+            (Operation.SNAPSHOT, t0, 5, "Marlon Brando"),
+            (Operation.SNAPSHOT, t0, 6, "Al Pacino"),
+            (Operation.SNAPSHOT, t0, 7, "James Caan"),
+            (Operation.SNAPSHOT, t0, 8, "Christian Bale"),
+            (Operation.SNAPSHOT, t0, 9, "Heath Ledger"),
+            (Operation.SNAPSHOT, t0, 10, "Mark Hamill"),
+            (Operation.SNAPSHOT, t0, 11, "Harrison Ford"),
+            (Operation.SNAPSHOT, t0, 12, "Carrie Fisher"),
+            (Operation.SNAPSHOT, t0, 13, "Robert Duvall"),
+            (Operation.SNAPSHOT, t0, 14, "John Marley"),
+            (Operation.SNAPSHOT, t0, 15, "Gary Oldman"),
         ],
-    [
-        # $operation, $load_date, id, name, year, director, rating, rank
-        (Operation.CREATE, t1, 1, "The Shawshank Redemption", 1994, "Frank Darabont", 9.3, 64),
-        (Operation.CREATE, t1, 2, "The Godfather", 1972, "Francis Ford Coppola", 9.2, 94),
-        (Operation.CREATE, t1, 3, "The Dark Knight", 2008, "Christopher Nolan", 9.0, 104),
-        (Operation.CREATE, t1, 4, "Star Wars: Episode V", 1980, "Irvin Kershner", 8.7, 485),
+        [
+            (Operation.CREATE, t2, 16, "John Travolta"),
+            (Operation.CREATE, t2, 17, "Liam Neeson"),
+            (Operation.CREATE, t2, 18, "Ralph Fiennes"),
+            (Operation.CREATE, t2, 19, "Ben Kingsley"),
+            (Operation.CREATE, t2, 20, "Leonardo DiCaprio")
+            (Operation.DELETE, t4, 13, "Robert Duvall"),
+        ],
+        [
+            (Operation.DELETE, t5, 14, "John Marley"),
+        ]
+    ]
 
-        (Operation.UPDATE, t2, 2, "The Godfather", 1972, "Francis Ford Coppola", 9.1, 91),
-        (Operation.UPDATE, t2, 3, "The Dark Knight", 2008, "Christopher Nolan", 8.9, 104),
-        (Operation.CREATE, t2, 5, "Pulp Fiction", 1994, "Quentin Terintino", 8.9, 138),
+    return [
+        spark.createDataFrame(b, [Columns.OPERATION, Columns.LOAD_DATE, "id", "name"]) \
+        .withColumn(Columns.HKEY, DataVaultFunctions.hash(["name", "year"])) for b in data
+    ]
 
-        (Operation.DELETE, t3, 1, "The Shawshank Redemption", 1994, "Frank Darabont", 9.3, 64),
-        (Operation.UPDATE, t4, 3, "The Dark Knight", 2008, "Christopher Nolan", 9.0, 101)
-    ],
-    [
-        (Operation.UPDATE, t5, 3, "The Dark Knight", 2008, "Christopher Nolan", 9.0, 101),
-        (Operation.DELETE, t3, 1, "The Shawshank Redemption", 1994, "Frank Darabont", 9.3, 64)
-    ]]
 
-    return spark \
-        .createDataFrame(data, [Columns.OPERATION, Columns.LOAD_DATE, "id", "name", "year", "director", "rating", "rank"]) \
-        .withColumn(Columns.HKEY, DataVaultFunctions.hash(["name", "year"]))
+def create_castings(spark: SparkSession) -> DataFrame:
+    t1 = datetime.now()
+    t0 = t1 - timedelta(days=1)
+    t2 = t1 + timedelta(hours=3, minutes=17)
+    t3 = t2 + timedelta(minutes=42) 
+    t4 = t3 + timedelta(hours=6)
+    t5 = t1 + timedelta(days=1, minutes=42)
+
+    data = [
+        [
+            # $operation, $load_date, movie_id, actor_id
+            (Operation.SNAPSHOT, t0, 1, 1),
+            (Operation.SNAPSHOT, t0, 1, 2),
+            (Operation.SNAPSHOT, t0, 1, 3),
+            (Operation.SNAPSHOT, t0, 1, 4),
+            (Operation.SNAPSHOT, t0, 2, 5),
+            (Operation.SNAPSHOT, t0, 2, 6),
+            (Operation.SNAPSHOT, t0, 2, 7),
+            (Operation.SNAPSHOT, t0, 3, 8),
+            (Operation.SNAPSHOT, t0, 3, 9),
+            (Operation.SNAPSHOT, t0, 4, 10),
+            (Operation.SNAPSHOT, t0, 4, 11),
+            (Operation.SNAPSHOT, t0, 4, 12),
+        ],
+        [
+            (Operation.CREATE, t2, 5, 16),
+            (Operation.CREATE, t2, 6, 17),
+            (Operation.CREATE, t2, 6, 18),
+            (Operation.CREATE, t2, 6, 19),
+            (Operation.CREATE, t2, 7, 20),
+        ],
+        []
+    ]
+
+    return [
+        spark.createDataFrame(b, [Columns.OPERATION, Columns.LOAD_DATE, "movie_id", "actor_id"]) \
+        .withColumn(Columns.HKEY, DataVaultFunctions.hash(["name", "year"])) for b in data
+    ]
 
 
 def prepare_hub(staging: DataFrame, business_key_columns: List[str]) -> DataFrame:
@@ -69,6 +161,7 @@ def prepare_hub(staging: DataFrame, business_key_columns: List[str]) -> DataFram
         .alias("l") \
         .groupBy("l.hkey", "l.name", "l.year") \
         .agg(F.min(F.col("l.load_date")).alias("load_date"), F.max(F.col("l.load_date")).alias("last_seen_date")) \
+
 
 def test_datavault_transformatios(spark: SparkSession):
     # Tabellen: Movies, Actors, Casts
@@ -97,6 +190,7 @@ def test_datavault_transformatios(spark: SparkSession):
     df_sat_actors = load_sat("HUB__movies", day01[0], business_key_columns=["name", "year"], current = df_sat_actors)
 
     # Assert updated content ...
+
 
 def test_transform(spark: SparkSession):
     df = create_movies(spark)
